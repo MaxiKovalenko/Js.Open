f37bd2ddea470c5bf6a74867767647a18941b549
MaxiKovalenko <48652119+MaxiKovalenko@users.noreply.github.com>
2019-05-26 22:27:36 +0300

    Update Lesson_7.js
    Доделал Домашнюю работу к уроку 7

--- Lesson_7/Lesson_7.js
+++ Lesson_7/Lesson_7.js
@@ -3,83 +3,71 @@ const products = [
     id: 1,
     name:'Ноутбук Samsung',
     price: 90000,
-    image: 'https://cdn1.ozone.ru/multimedia/c500/1023907415.jpg',
+    image: 'https://img.mvideo.ru/Pdb/30014706b.jpg',
   },
   {
     id: 2,
     name:'Ноутбук HP',
     price: 110000,
-    image: 'https://items.s1.citilink.ru/1110249_v02_s.jpg',
+    image: 'https://andpro.ru/upload/iblock/695/90f8b072_90df_11e8_80ea_001e67d1aaeb_90f8b073_90df_11e8_80ea_001e67d1aaeb.jpg',
   },
-  
-]
+];
 
-var cart = [];
+ var cart = [];
 
 function buildCatalog() {
   var $catalog = document.querySelector('#catalog');
   
-  for(var i = 0; i< products.length; i++) {
+  for(var i = 0; i < products.length; i++) {
     var $template = document.querySelector('#template').children[0].cloneNode(true);
     $template.querySelector('.title').textContent = products[i].name;
     $template.querySelector('.price').textContent = products[i].price;
     $template.querySelector('.picture').src = products[i].image;
-    
+     
      $template.querySelector('.buy').dataset.id = products[i].id;
-     $template.querySelector('.buy').dataset.name = products[i].name;
-     $template.querySelector('.buy').dataset.price = products[i].price;
-     $template.querySelector('.buy').dataset.image = products[i].image;
+      $template.querySelector('.buy').dataset.name = products[i].name;
+      $template.querySelector('.buy').dataset.price = products[i].price;
+      $template.querySelector('.buy').dataset.image = products[i].image;
      
      $catalog.appendChild($template);
   }
 }
 
-
-function isExist(id) {
+ function isExist(id) {
   for(var i = 0; i < cart.length; i++) {
-    if (cart[i].id === id) {
-      return true;
-    }
-  }
-  return false;
+     if (cart[i].id === id) {
+       return true;
+     }  
 }
+ return false;
+ }
 
+ function findIdx(id) {
+   for (var i = 0; i < cart.length; i++) {
+     if (cart[i].id === id) {
+       return i;
+     }
+   }
+ }
 
-function findIdx(id) {
-  for (var i = 0; i < cart.length; i++) {
-    if (cart[i].id === id) {
-      return i;
-    }
+  function handleBuyClick(event) {
+   if (event.target.tagName === 'BUTTON') {
     
+     if (isExist(+event.target.dataset.id)) {
+       var idx = findIdx(+event.target.dataset.id);
+       cart[idx].quantity++;
+     } else {
+       cart.push({
+         id: +event.target.dataset.id,
+         name: event.target.dataset.name,
+         price: +event.target.dataset.price,
+         image: event.target.dataset.image,
+         quantity: 1,
+         });
+     }
+     countCartTotal(cart);
+   }
   }
-}
-
-
-function handleBuyClick(event) {
-  if (event.target.dagName === 'BUTTON') {
-    if (isExist(+event.target.dataset.id)) {
-      var idx = findIdx(+event.target.dataset.id);
-      $cart[idx].quantity++;
-      
-    } else {
-      cart.push({
-        id: +event.target.dataset.id,
-        name: event.target.dataset.name,
-        price: +event.target.dataset.price,
-        image: event.target.dataset.image,
-        quantity: 1,
-      });
-    }
-    countCartTotal(cart);
-  }
-  
-  if (event.target.tagName === 'imge') {
-    var $overlay = document.querySelector('.overlay');
-    $overlay.style.display = 'block';
-    
-    $overlay.querySelector('.preview').src = event.target.src;
-  }
-}
 
 function countCartTotal(items) {
    var $cart = document.getElementById('cart');
@@ -102,21 +90,16 @@ function countCartTotal(items) {
    $cart.appendChild($item);
    
    total += items[i].quantity * items[i].price
-     }
+   }
      var $total = document.createElement('div');
-     $total.textContent = 'Общая сумма покупки ' + total;
+     $total.textContent = 'Общая сумма покупки: ' + total;
      $cart.appendChild($total);
    }  else {
     $cart.textContent = 'Корзина пуста';
   }
 }
  
-function handleCloseClick() {
-  var $overlay = document.querySelector('.overlay');
-  $overlay.style.display = 'none';
-}
-
-function handleRemoveClick(event) {
+ function handleRemoveClick(event) {
   if (event.target.classList.contains('remove')) {
     var idx = findIdx(+event.target.dataset.id);
    
@@ -129,36 +112,16 @@ function handleRemoveClick(event) {
    }
     countCartTotal(cart); 
   }
-  
-}
-
-function handleTabClick(event) {
-  event.preventDefault();
-  if (event.target.classList.contains('tab')) {
-    var $contents = document.getElementsByClassName('content');
-    for (var i = 0; i < $contents.length; i++) {
-      $contents[i].classList.remove('active');
-      
-    }
-    var id = event.target.getAttribute('href');
-    document.querySelector(id).classList.add('active');
-  }
-}
+ }
 
 function init() {
-  buildCatalog();
-  countCartTotal(cart);
+   buildCatalog();
+   countCartTotal(cart);
   
-  var $catalog = document.querySelector('#catalog');
-  $catalog.addEventListener('click', handleBuyClick);
+   var $catalog = document.querySelector('#catalog');
+   $catalog.addEventListener('click', handleBuyClick);
   
-  var $close = document.querySelector('.close');
-  $close.addEventListener('click', handleCloseClick);
-  
-  var $cart = document.getElementById('cart');
-  $cart.addEventListener('click', handleRemoveClick);
- 
-  var $accordion = document.getElementById('accordion');
-  $accordion.addEventListener('click', handleTabClick)
-}
+   var $cart = document.getElementById('cart');
+   $cart.addEventListener('click', handleRemoveClick);
+   }
 window.addEventListener('load', init);
